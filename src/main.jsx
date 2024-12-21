1eedd8c71bde973dd08de372553d39cf6cd9b093
MacBook Air <macbookair@kenethescobar.local>
2024-12-21 13:11:07 -0500

    Creacion de la funcionalidad PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client"; // Usa ReactDOM desde react-dom/client
 import App from "./App";
 
+import ReloadPrompt from "../ReloadPrompt";
+
 const root = ReactDOM.createRoot(document.getElementById("root")); // Crea el punto de entrada raíz
 root.render(
   <React.StrictMode>
     <App />
+    <ReloadPrompt/>
   </React.StrictMode>
 );
